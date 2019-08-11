6948d983fe947813070bd57afe6575de55e98296
poornima S <poornii87@gmail.com>
2019-08-11 07:57:15 +0100

    HomeProject5Commit

---
+++ src/practiseProgramming/Class5.java
@@ -0,0 +1,13 @@
+//Creating Package
+ package practiseProgramming;
+//Creating class
+public class Class5 {
+    public static void main(String[] args) {
+        //Print function
+        System.out.println("Hi");
+    }
+    //End of main function
+}
+//End of class5
+//End of program
+
